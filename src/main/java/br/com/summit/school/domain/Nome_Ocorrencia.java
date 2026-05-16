0633fa553f7b1c02a71f40fc357d99d4ecd7adef
Weslley Rocha <weslley.nx@gmail.com>
2026-05-16 15:59:38 -0300

    Adicionando Tipo_Ocorrencia

---
+++ src/main/java/br/com/summit/school/domain/Nome_Ocorrencia.java
@@ -0,0 +1,19 @@
+package br.com.summit.school.domain;
+
+public enum Nome_Ocorrencia {
+
+    ADMINISTRATIVO,
+    DISCIPLINAR,
+    PEDAGOGICA,
+    MEDICA,
+    ACADEMICA,
+    COMPORTAMENTAL,
+    FINANCEIRA,
+    OPERACIONAL,
+    PSICOLOGICA,
+    INCIDENTE,
+    INDISCIPLINAR,
+    ATRASO,
+    RENDIMENTO
+}
+
---
+++ src/main/java/br/com/summit/school/domain/Tipo_Ocorrencia.java
@@ -0,0 +1,32 @@
+package br.com.summit.school.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "Tipo_Ocorrencia")
+@Table(name = "Tipo_Ocorrencia")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@EqualsAndHashCode(of = "id_tipo_ocorrencia")
+public class Tipo_Ocorrencia {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_tipo_ocorrencia;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "nome_ocorrencia")
+    private Nome_Ocorrencia nome_Ocorrencia;
+
+}
