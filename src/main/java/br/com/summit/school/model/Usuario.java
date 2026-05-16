ae005c5fe4f2cb4e28466824364fc2d97893fa6b
Lyncoln Santiago <lyncolnsantiago7@gmail.com>
2026-05-16 14:03:23 -0300

    Change senha column length from 255 to 155

--- src/main/java/br/com/summit/school/model/Usuario.java
+++ src/main/java/br/com/summit/school/model/Usuario.java
@@ -27,7 +27,7 @@ public class Usuario {
     @Column(nullable = false, unique = true, length = 155)
     private String login;
 
-    @Column(nullable = false, length = 255)
+    @Column(nullable = false, length = 155)
     private String senha;
 
     @ManyToOne(fetch = FetchType.EAGER)
