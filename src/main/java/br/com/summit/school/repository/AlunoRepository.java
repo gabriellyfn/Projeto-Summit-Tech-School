e87981380d65d75f192a7192354807e8b28e973c
Gabrielly <gaby.gdmc@gmail.com>
2026-05-16 15:16:02 -0300

    fix: Interface Aluno Repository

--- src/main/java/br/com/summit/school/repository/AlunoRepository.java
+++ src/main/java/br/com/summit/school/repository/AlunoRepository.java
@@ -1,4 +1,10 @@
 package br.com.summit.school.repository;
 
-public class AlunoRepository {
+import br.com.summit.school.model.Aluno;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AlunoRepository extends JpaRepository<Aluno, Long> {
+
 }
