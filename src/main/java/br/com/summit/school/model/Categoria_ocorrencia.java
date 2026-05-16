7259754f76247aabcde66939faf319ac6b85ed23
Weslley Rocha <weslley.nx@gmail.com>
2026-05-16 12:01:05 -0300

    fix: Adicionando Ocorrencia.java

---
+++ src/main/java/br/com/summit/school/model/Categoria_ocorrencia.java
@@ -0,0 +1,11 @@
+package br.com.summit.school.model;
+
+public enum Categoria_ocorrencia {
+
+    DISCIPLINA,
+    FALTA,
+    ATRASO,
+    COMPORTAMENTO,
+    RENDIMENTO,
+    OUTROS,
+}
---
+++ src/main/resources/db.migration/V4__created-table-ocorrencia.sql
@@ -0,0 +1,17 @@
+CREATE TABLE ocorrencia
+(
+    id_ocorrencia      BIGINT       NOT NULL AUTO_INCREMENT,
+    id_turma           BIGINT       NOT NULL,
+    id_aluno           BIGINT       NOT NULL,
+    id_usuario         BIGINT       NOT NULL,
+    id_tipo_ocorrencia BIGINT       NOT NULL,
+    data               DATE         NOT NULL,
+    hora               TIME         NOT NULL,
+    descricao          VARCHAR(255) NOT NULL,
+
+    PRIMARY KEY (id_ocorrencia),
+    CONSTRAINT fk_id_turma FOREIGN KEY (id_turma) REFERENCES turmas (id_turma),
+    CONSTRAINT fk_id_aluno FOREIGN KEY (id_aluno) REFERENCES alunos (id_aluno),
+    CONSTRAINT fk_id_usuario FOREIGN KEY (id_usuario) REFERENCES usuarios (id_usuario),
+    CONSTRAINT fk_id_tipo_ocorrencia FOREIGN KEY (id_tipo_ocorrencia) REFERENCES tipos_ocorrencia (id_tipo_ocorrencia)
+);
