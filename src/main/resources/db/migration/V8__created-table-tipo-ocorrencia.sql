55124db94371654d48dfb3b37d7de3eef0013566
gabriellyfn <gaby.gdmc@gmail.com>
2026-05-24 21:43:09 -0300

    feat: Criação da V8 migration que alimenta o banco de dados do Enum

--- src/main/resources/db/migration/V8__created-table-tipo-ocorrencia.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE tipo_ocorrencia
-
-(
-    id_tipo_ocorrencia BIGINT      NOT NULL AUTO_INCREMENT,
-    nome_ocorrencia    VARCHAR(50) NOT NULL,
-
-    PRIMARY KEY (id_tipo_ocorrencia)
-);
