8cfd8b3a9e42d9de2132510e6379e926f97f5b18
Weslley Rocha <weslley.nx@gmail.com>
2026-05-25 19:27:52 -0300

    Atualizando Migration

--- src/main/resources/db/migration/V8__popular_tabela_perfil.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO Perfil (id_perfil, nome_perfil) VALUES (1, 'PROFESSOR');
-INSERT INTO Perfil (id_perfil, nome_perfil) VALUES (2, 'COORDENADOR');
-INSERT INTO Perfil (id_perfil, nome_perfil) VALUES (3, 'ANALISTA_DE_QUALIDADE');
-INSERT INTO Perfil (id_perfil, nome_perfil) VALUES (4, 'PROFESSOR_ADMINISTRATIVO');
