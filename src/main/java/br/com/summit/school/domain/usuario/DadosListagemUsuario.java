608d6096a607b8b4914fc8ba03b24a7c541e1f11
gabriellyfn <gaby.gdmc@gmail.com>
2026-05-26 21:46:55 -0300

    feat/refactor: Adiciona DTO Listagem de Usuarios e Refatoração de UsuarioController

---
+++ src/main/java/br/com/summit/school/domain/usuario/DadosListagemUsuario.java
@@ -0,0 +1,23 @@
+package br.com.summit.school.domain.usuario;
+
+public record DadosListagemUsuario(
+        Long id,
+        String nome,
+        String login,
+        String email,
+        String telefone,
+        String foto,
+        String perfil
+) {
+    public DadosListagemUsuario(Usuario usuario){
+        this(
+                usuario.getId_usuario(),
+                usuario.getNome(),
+                usuario.getLogin(),
+                usuario.getEmail(),
+                usuario.getTelefone(),
+                usuario.getFoto(),
+                usuario.getPerfil().getNome().toString()
+        );
+    }
+}
