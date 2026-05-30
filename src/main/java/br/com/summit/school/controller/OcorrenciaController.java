30b830c48860bb80a0ba89e6dd4aa9721fb6788b
Gabrielly <gaby.gdmc@gmail.com>
2026-05-30 15:46:49 -0300

    fix: remover imports

--- src/main/java/br/com/summit/school/controller/OcorrenciaController.java
+++ src/main/java/br/com/summit/school/controller/OcorrenciaController.java
@@ -1,13 +1,25 @@
 package br.com.summit.school.controller;
 
-import br.com.summit.school.domain.ocorrencia.*;
+
+import br.com.summit.school.domain.ocorrencia.DadosAtualizacaoOcorrencia;
+import br.com.summit.school.domain.ocorrencia.DadosCadastroOcorrencia;
+import br.com.summit.school.domain.ocorrencia.DadosDetalhamentoOcorrencia;
+import br.com.summit.school.domain.ocorrencia.DadosListagemOcorrencia;
+import br.com.summit.school.domain.ocorrencia.OcorrenciaService;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
