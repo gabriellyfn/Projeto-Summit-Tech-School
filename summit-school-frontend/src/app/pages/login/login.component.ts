3286dfc3fb00a802697d3eaa6f29803a8c95593c
Weslley Rocha <weslley.nx@gmail.com>
2026-05-29 23:07:12 -0300

    Removendo teste de login !

--- summit-school-frontend/src/app/pages/login/login.component.ts
+++ summit-school-frontend/src/app/pages/login/login.component.ts
@@ -28,27 +28,18 @@ export class LoginComponent {
   }
 
   onSubmit() {
-    // 1. Log para garantir que o método está sendo chamado
-    console.log('Botão clicado! Validando formulário...');
-
-    // 2. Verifica se é válido
     if (this.loginForm.valid) {
       this.showFormError = false;
-      console.log('Formulário válido. Enviando dados para API:', this.loginForm.value);
 
       this.authService.login(this.loginForm.value).subscribe({
         next: (response) => {
-          console.log('Resposta da API (Sucesso):', response);
-          alert('Login efetuado com sucesso!'); // Feedback visual temporário
           // this.router.navigate(['/dashboard']);
         },
         error: (err) => {
-          console.error('Resposta da API (Erro):', err);
           alert('Falha no login. Verifique suas credenciais.');
         }
       });
     } else {
-      console.log('Formulário inválido. Exibindo mensagens de erro.');
       this.loginForm.markAllAsTouched();
       this.showFormError = true;
     }
