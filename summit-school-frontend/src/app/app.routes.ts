f2efcb63a4626ad3cb310c88589a7d086e0c1aaa
Weslley Rocha <weslley.nx@gmail.com>
2026-05-30 11:54:38 -0300

    Refatorando Tela ocorrencia.service.ts

--- summit-school-frontend/src/app/app.routes.ts
+++ summit-school-frontend/src/app/app.routes.ts
@@ -1,14 +1,13 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { AdminComponent } from './pages/admin/admin.component';
 import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  {
-    path: 'dashboard',
-    component: DashboardComponent,
-    canActivate: [authGuard] // Aplica o guard para proteger a rota
-  }
+  { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] },
+  { path: 'admin/config', component: AdminComponent, canActivate: [authGuard] },
+  { path: '**', redirectTo: 'login' }
 ];
